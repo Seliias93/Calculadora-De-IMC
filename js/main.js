bf3df39d22e6f9302226b59cd609f223172033db
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-03-15 12:11:26 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Capturar evento de submit do formulário
+ // Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
